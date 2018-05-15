cb060489974691d541cc20983a2cae4f9926bcee
Harutyun <harutandartur@mail.ru>
2018-05-15 21:54:14 +0400

    First commit.

---
+++ project.py
@@ -0,0 +1,45 @@
+import sys
+import re
+import subprocess
+import shlex
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--testPath', help = 'Path of test data')
+parser.add_argument('--modelPath', help = 'Path of trained model')
+
+parsedArgs = parser.parse_args()
+
+testPath = parsedArgs.testPath
+modelPath = parsedArgs.modelPath
+
+
+testFile = open(testPath, 'r')
+testData = testFile.read()
+
+labels = re.findall('__label__[1-9]{1,}', testData)
+rows = filter(None, re.split('__label__[1-9]{1,}', testData))
+
+command = shlex.split("../fastText/fastText-0.1.0/./fasttext predict-prob " + modelPath  + " " + testPath)
+
+process = subprocess.Popen(command, stdout=subprocess.PIPE)
+output, err = process.communicate()
+
+receivedLabels = re.findall('__label__[1-9]{1,}', output)
+
+labels = np.array(labels)
+receivedLabels = np.array(receivedLabels)
+
+differences = [i != j for i, j in zip(labels, receivedLabels)]
+differences = np.array(differences, dtype = np.uint8)
+#print np.count_nonzero(differences)
+#print float(len(differences) - np.count_nonzero(differences))/len(differences)
+
+print np.nonzero(differences)[0][:5]
+for i in np.nonzero(differences)[0][:5]:
+	print rows[i]
+
+#print output
+
